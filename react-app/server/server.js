9198b5f3417fc7f4a91e97f31fca5c1185c337ea
Krishna Patel <krishnapatel@krishnas-mbp.attlocal.net>
2022-12-18 18:24:00 -0800

    Cleanup

--- react-app/server/server.js
+++ react-app/server/server.js
@@ -1,10 +1,15 @@
 //Creating an express server
 const express = require('express');
-let app = express();
+const app = express();
+const path = require("path");
 
 let port = process.env.port || 8081;
-
+app.use(express.static(path.join(__dirname, "..", "build")));
 app.use(express.static("public"));
+
+app.use((req, res, next) => {
+    res.sendFile(path.join(__dirname, "..", "build", "index.html"));
+  });
 
 app.get('/test', function (req,res){
     res.send('Smoke Test!');
---
+++ react-app/src/components/mainPage.jsx
@@ -0,0 +1,12 @@
+import React,{Component} from 'react';
+
+export default class MainPage extends Component {
+  
+    render() {
+      return (
+        <div className='mainPage'>
+          <h1>Main</h1>
+        </div>
+      );
+    }
+  }
